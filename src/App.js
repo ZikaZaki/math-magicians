eaa2ff719446947539c7d7002fbd08d6215f1f70
ZikaZaki <zika.github@gmail.com>
2022-09-22 01:48:37 +0300

    Add Calculator Class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import CalcWrapper from './components/CalcWrapper';
-import CalcScreen from './components/CalcScreen';
-import ButtonsPad from './components/ButtonsPad';
-import Button from './components/Button';
-/* eslint-disable react/no-array-index-key */
+import Calculator from './components/Calculator';
+/* eslint-disable react/prefer-stateless-function */
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.btnValues = [
-      ['AC', '+/-', '%', '÷'],
-      ['7', '8', '9', 'x'],
-      ['4', '5', '6', '-'],
-      ['1', '2', '3', '+'],
-      ['0', '.', '='],
-    ];
-  }
-
   render() {
     return (
-      <CalcWrapper>
-        <CalcScreen value="0" />
-        <ButtonsPad>
-          {this.btnValues.flat().map((row, index) => (
-            <Button key={index} value={row} />
-          ))}
-        </ButtonsPad>
-      </CalcWrapper>
+      <>
+        <Calculator />
+      </>
     );
   }
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import CalcWrapper from './CalcWrapper';
+import CalcScreen from './CalcScreen';
+import ButtonsPad from './ButtonsPad';
+import Button from './Button';
+/* eslint-disable react/no-array-index-key */
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.btnValues = [
+      ['AC', '+/-', '%', '÷'],
+      ['7', '8', '9', 'x'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '='],
+    ];
+  }
+
+  render() {
+    return (
+      <CalcWrapper>
+        <CalcScreen value="0" />
+        <ButtonsPad>
+          {this.btnValues.flat().map((row, index) => (
+            <Button key={index} value={row} />
+          ))}
+        </ButtonsPad>
+      </CalcWrapper>
+    );
+  }
+}
+
+export default Calculator;
